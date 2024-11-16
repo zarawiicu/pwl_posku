72d61c856ec1195beb3b38ef2bdf3c8705c8d898
zarawiicu <zarasaa05@gmail.com>
2024-11-16 15:00:45 +0700

    RESTFUL API: Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,15 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\LevelModel; // {{ edit_1 }}
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+// use illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends Model
+class UserModel extends Model implements JWTSubject
 {
     use HasFactory;
+
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['username', 'nama', 'password', 'level_id'];
 
     public function level(): BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
